de1c0ee82440d9a40f67f6495f4a861557859ecb
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-08-16 17:09:54 +0200

    fully fleged passwiord gen

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from utils.passwordgen import PasswordGen
 print("Welcome To Cracker 601\nDeveloped By Openchains\n")
-initials = input("Enter initiatials: ")
-postFixs = input("Input PostFixs")
+initials = input("Enter initiatials separatebySpace: ")
 year = input("input year: ")
-characters = input("input characters")
+characters = input("input characters: ")
 fileName = input("Enter File Name: ")
 passGen = PasswordGen();
 passGen.setCharacterSet(characters);
 passGen.setInitials(initials)
-passGen.setPostFixs(postFixs)
 passGen.setYear(year)
 passGen.generateList(fileName.strip())
+print(f"File {fileName.strip()} successfully saved")
--- utils/passwordgen.py
+++ utils/passwordgen.py
@@ -1,17 +1,19 @@
 from itertools import combinations
+def lowercase_first_letter(input_string):
+    if not input_string:  # Handle empty string case
+        return input_string
+    return input_string[0].lower() + input_string[1:]
 class PasswordGen:
     def __init__(self):
         self.year : str = ""
-        self.initials : str = ""
-        self.postFixs : str = "";
+        self.initials : list[str] = ""
         self.characterSet : list[str] = []
-        
     def setYear(self,year : str):
         self.year = year
     def setInitials(self,initials: str):
-        self.initials = initials
-    def setPostFixs(self,postFixs : str):
-        self.postFixs = postFixs;
+        init0 = map( lambda x : x.capitalize(),initials.split(' '))
+        init1 = map( lambda x : lowercase_first_letter(x),initials.split(' '))
+        self.initials = [*init0,*init1]
     def setCharacterSet(self,characters : str):
         self.characterSet = list(characters)
     def generateList(self,outputFile):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
@@ -19,10 +21,11 @@ class PasswordGen:
             listPassKeys = self._getFirstPasswordSuggestion()
             for i in range(len(listPassKeys)):
                 file.write(listPassKeys[i]+"\n")
+            file.close()
             pass
         pass
     def _getFirstPasswordSuggestion(self):
-        array : list[str] = [self.year,self.initials,self.postFixs,*self.characterSet]
+        array : list[str] = [self.year,*self.initials,*self.characterSet]
         possiblePasswords : list[str] = []
         for r in range(1, len(array) + 1):
             for c in combinations(array, r):
@@ -33,4 +36,3 @@ class PasswordGen:
                 if reversed_c != c:
                     possiblePasswords.append("".join(map(str, reversed_c)))
         return possiblePasswords
-
