5b4daabccf8432b86657cdd4f6be660ea6721f1f
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-08-16 16:43:30 +0200

    firts [assgen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from utils.passwordgen import PasswordGen
 print("Welcome To Cracker 601\nDeveloped By Openchains\n")
 initials = input("Enter initiatials: ")
-postDixs = input("Input PostFixs")
+postFixs = input("Input PostFixs")
 year = input("input year: ")
 characters = input("input characters")
+fileName = input("Enter File Name: ")
 passGen = PasswordGen();
 passGen.setCharacterSet(characters);
 passGen.setInitials(initials)
 passGen.setPostFixs(postFixs)
 passGen.setYear(year)
+passGen.generateList(fileName.strip())
